c4a8a0eaec4be5a1e4acf97deb846519c346614e
Anshal kumar <anshalkumar900@gmail.com>
2025-03-01 16:20:55 +0000

    Sending json response

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include <netinet/in.h> 
 #include <unistd.h> 
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+
+
+
 
 class Server{
     public:     
@@ -15,7 +19,32 @@ class Server{
         struct sockaddr_in address;
         int addrlen = sizeof(address);
         char buffer[BUFFER_SIZE] = {0};
+    
+        string responsestring(size_t length , const string &content, string &contentType) {
+           // Send response
+            string response = 
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: " + contentType + "\r\n"
+                "Content-Length: " + to_string(length) + "\r\n"
+                "\r\n" + 
+                content;  
 
+            return response;
+        };
+
+    void SendJson(const char *contents){
+
+        string type = "application/json";
+        string response = responsestring(strlen(contents) , string(contents) ,  type);
+
+        send(client_socket, response.c_str(), response.length(), 0);
+                  
+        
+    };
+
+
+    
+   
     int CreateServer(){
     
             server_fd = socket(AF_INET , SOCK_STREAM, 0);
@@ -27,7 +56,7 @@ class Server{
 
     Server(){
             address.sin_family = AF_INET; //IPV4 
-            address.sin_port = htons(PORT); //Server listens on this port and htons for Convert to network byte order
+            address.sin_port = htons(PORT); //Server listens on this port and htons for Converting to network byte order
             address.sin_addr.s_addr = INADDR_ANY; //Accept connections from all intefaces wifi , loopback or ethernet.
             
             if(CreateServer() == 1){
@@ -44,7 +73,7 @@ class Server{
             // Start listening (max 3 clients in queue)
             if (listen(server_fd, 3) < 0) {
                 perror("Listen failed");
-                exit(EXIT_FAILURE);
+                exit(1);
             };            
             
             cout << "Listening on port " << PORT << endl;
@@ -68,15 +97,9 @@ class Server{
             read(client_socket , buffer,  BUFFER_SIZE);
             cout << "Received request: \n" << buffer << endl;
                 
-
-            // Send response
-            string response = 
-                "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/html\r\n"
-                "Content-Length: 13\r\n"
-                "\r\n"
-                "Hello, World!";
-            send(client_socket, response.c_str(), response.length(), 0);
+           
+            
+            SendJson(R"({"message" : "This is anshal"})");  
 
             // Close connection
             close(client_socket);
@@ -84,6 +107,9 @@ class Server{
         };
     };
 };
+
+
+
 
 int main(int argc , char *argv[]){
     
